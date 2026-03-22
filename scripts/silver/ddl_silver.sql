c61de9a58195a4b1ca65524fac959dd86f4a00be
Sohaib Atef <sohaib.aatef@gmail.com>
2026-03-22 13:42:36 +0200

    Updated the structure of the ddl based on the new transformations

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -14,51 +14,52 @@ Script Purpose:
 -- Creating tables for each file...
 -- 1.1. cust_info.csv
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+    DROP TABLE [silver].[crm_cust_info];
 GO
-
-CREATE TABLE silver.crm_cust_info (
+CREATE TABLE [silver].[crm_cust_info] (
     cst_id              INT
-    ,cst_key            NVARCHAR(50)
-    ,cst_firstname      NVARCHAR(50)
-    ,cst_lastname       NVARCHAR(50)
-    ,cst_marital_status NVARCHAR(50)
-    ,cst_gndr           NVARCHAR(50)
+    ,cst_key            NVARCHAR (50)
+    ,cst_firstname      NVARCHAR (50)
+    ,cst_lastname       NVARCHAR (50)
+    ,cst_marital_status NVARCHAR (50)
+    ,cst_gndr           NVARCHAR (50)
     ,cst_create_date    DATE
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
 
 -- 1.2. prd_info.csv
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
+    DROP TABLE [silver].[crm_prd_info];
 GO
-
-CREATE TABLE silver.crm_prd_info (
+CREATE TABLE [silver].[crm_prd_info] (
     prd_id              INT
-    ,prd_key            NVARCHAR(50)
-    ,prd_nm             NVARCHAR(50)
+    ,cat_id             NVARCHAR (50)
+    ,prd_key            NVARCHAR (50)
+    ,prd_nm             NVARCHAR (50)
     ,prd_cost           INT
-    ,prd_line           NVARCHAR(50)
-    ,prd_start_dt       DATETIME
-    ,prd_end_dt         DATETIME
+    ,prd_line           NVARCHAR (50)
+    ,prd_start_dt       DATE
+    ,prd_end_dt         DATE
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
 
 -- 1.3. sales_details.csv
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE silver.crm_sales_details;
+    DROP TABLE [silver].[crm_sales_details];
 GO
-
-CREATE TABLE silver.crm_sales_details (
-    sls_ord_num         NVARCHAR(50)
-    ,sls_prd_key        NVARCHAR(50)
+CREATE TABLE [silver].[crm_sales_details] (
+    sls_ord_num         NVARCHAR (50)
+    ,sls_prd_key        NVARCHAR (50)
     ,sls_cust_id        INT
-    ,sls_order_dt       INT
-    ,sls_ship_dt        INT
-    ,sls_due_dt         INT
+    ,sls_order_dt       DATE
+    ,sls_ship_dt        DATE
+    ,sls_due_dt         DATE
     ,sls_sales          INT
     ,sls_quantity       INT
     ,sls_price          INT
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
 
@@ -70,36 +71,36 @@ GO
 -- Creating tables for each file...
 -- 2.1. CUST_AZ12.csv
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE silver.erp_cust_az12;
+    DROP TABLE [silver].[erp_cust_az12];
 GO
-
-CREATE TABLE silver.erp_cust_az12 (
-    cid                 NVARCHAR(50)
+CREATE TABLE [silver].[erp_cust_az12] (
+    cid                 NVARCHAR (50)
     ,bdate              DATE
-    ,gen                NVARCHAR(50)
+    ,gen                NVARCHAR (50)
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
 
 -- 2.2. LOC_A101.csv
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE silver.erp_loc_a101;
+    DROP TABLE [silver].[erp_loc_a101];
 GO
-
-CREATE TABLE silver.erp_loc_a101 (
-    cid                 NVARCHAR(50)
-    ,cntry              NVARCHAR(50)
+CREATE TABLE [silver].[erp_loc_a101] (
+    cid                 NVARCHAR (50)
+    ,cntry              NVARCHAR (50)
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
 
 -- 2.3. PX_CAT_G1V2.csv
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE silver.erp_px_cat_g1v2;
+    DROP TABLE [silver].[erp_px_cat_g1v2];
 GO
-
-CREATE TABLE silver.erp_px_cat_g1v2 (
-    id                  NVARCHAR(50)
-    ,cat                NVARCHAR(50)
-    ,subcat             NVARCHAR(50)
-    ,maintenance        NVARCHAR(50)
+CREATE TABLE [silver].[erp_px_cat_g1v2] (
+    id                  NVARCHAR (50)
+    ,cat                NVARCHAR (50)
+    ,subcat             NVARCHAR (50)
+    ,maintenance        NVARCHAR (50)
+    ,dwh_create_date    DATETIME2       DEFAULT GETDATE()
 );
 GO
