493277333e968ed4b31d0bafeac4e33a4f4b5b5e
Sohaib Atef <sohaib.aatef@gmail.com>
2026-03-26 20:33:05 +0200

    Updated some code order by making the "Go" keyword as an opening for the next block of SQL code insetead of making it a closure of the previous code!

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -15,8 +15,8 @@ Script Purpose:
 -- 1.1. cust_info.csv
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
-GO
 
+GO
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT
     ,cst_key            NVARCHAR(50)
@@ -26,13 +26,13 @@ CREATE TABLE bronze.crm_cust_info (
     ,cst_gndr           NVARCHAR(50)
     ,cst_create_date    DATE
 );
-GO
 
+GO
 -- 1.2. prd_info.csv
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
-GO
 
+GO
 CREATE TABLE bronze.crm_prd_info (
     prd_id              INT
     ,prd_key            NVARCHAR(50)
@@ -42,13 +42,13 @@ CREATE TABLE bronze.crm_prd_info (
     ,prd_start_dt       DATETIME
     ,prd_end_dt         DATETIME
 );
-GO
 
+GO
 -- 1.3. sales_details.csv
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
-GO
 
+GO
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num         NVARCHAR(50)
     ,sls_prd_key        NVARCHAR(50)
@@ -71,35 +71,34 @@ GO
 -- 2.1. CUST_AZ12.csv
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
-GO
 
+GO
 CREATE TABLE bronze.erp_cust_az12 (
     cid                 NVARCHAR(50)
     ,bdate              DATE
     ,gen                NVARCHAR(50)
 );
-GO
 
+GO
 -- 2.2. LOC_A101.csv
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
-GO
 
+GO
 CREATE TABLE bronze.erp_loc_a101 (
     cid                 NVARCHAR(50)
     ,cntry              NVARCHAR(50)
 );
-GO
 
+GO
 -- 2.3. PX_CAT_G1V2.csv
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
-GO
 
+GO
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id                  NVARCHAR(50)
     ,cat                NVARCHAR(50)
     ,subcat             NVARCHAR(50)
     ,maintenance        NVARCHAR(50)
 );
-GO
